3cd42e3e04b85d03ffde5db577c77a7b6d63927f
rtiwari73 <tiwariravi@gmail.com>
2025-01-08 12:07:58 -0500

    First change

---
+++ function_app.py
@@ -0,0 +1,27 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="orderTrigger")
+def orderTrigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    a=2
+    b=3
+    c=a+b
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             f"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. the sume of a +b is {c}",
+             status_code=200
+        )
